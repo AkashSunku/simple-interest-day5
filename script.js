2ee63717d7cf12f1b3854879569424bac56b0285
Akash Sunku <akash.sunku1234@gmail.com>
2021-05-04 15:42:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function Calculate(){
+   p= document.getElementById("p").value;
+
+   r= document.getElementById("r").value;
+
+   t= document.getElementById("t").value;
+   result = (p*t*r)/100;
+
+  document.getElementById("demo").innerHTML= "The Interest is: " + result;
+}
